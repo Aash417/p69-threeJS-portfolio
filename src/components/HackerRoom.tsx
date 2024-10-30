f3fe5b792729f7e33d22a5f87a688c83716be405
aashish_k <aashishkathat00@gmail.com>
2024-10-30 21:31:22 +0530

    fix bugs

--- src/components/HackerRoom.tsx
+++ src/components/HackerRoom.tsx
@@ -9,123 +9,124 @@ import * as THREE from 'three';
 import { isMesh } from '../constants/helper';
 
 type HackerRoomProps = {
-	scale: number;
-	position: [number, number, number] | THREE.Vector3;
-	rotation: [number, number, number];
+   scale: number;
+   position: [number, number, number] | THREE.Vector3;
+   rotation: [number, number, number];
 };
+
 export default function HackerRoom(props: HackerRoomProps) {
-	const { nodes, materials } = useGLTF('/models/hacker-room.glb');
+   const { nodes, materials } = useGLTF('/models/hacker-room.glb');
 
-	const monitortxt = useTexture('textures/desk/monitor.png');
-	const screenTxt = useTexture('textures/desk/screen.png');
+   const monitortxt = useTexture('textures/desk/monitor.png');
+   const screenTxt = useTexture('textures/desk/screen.png');
 
-	return (
-		<group {...props} dispose={null}>
-			{isMesh(nodes.screen_screens_0) && (
-				<mesh
-					geometry={nodes.screen_screens_0.geometry}
-					material={materials.screens}
-				>
-					<meshMatcapMaterial map={screenTxt} />
-				</mesh>
-			)}
+   return (
+      <group {...props} dispose={null}>
+         {isMesh(nodes.screen_screens_0) && (
+            <mesh
+               geometry={nodes.screen_screens_0.geometry}
+               material={materials.screens}
+            >
+               <meshMatcapMaterial map={screenTxt} />
+            </mesh>
+         )}
 
-			{isMesh(nodes.screen_glass_glass_0) && (
-				<mesh
-					geometry={nodes.screen_glass_glass_0.geometry}
-					material={materials.glass}
-				/>
-			)}
+         {isMesh(nodes.screen_glass_glass_0) && (
+            <mesh
+               geometry={nodes.screen_glass_glass_0.geometry}
+               material={materials.glass}
+            />
+         )}
 
-			{isMesh(nodes.table_table_mat_0_1) && (
-				<mesh
-					geometry={nodes.table_table_mat_0_1.geometry}
-					material={materials.table_mat}
-				/>
-			)}
+         {isMesh(nodes.table_table_mat_0_1) && (
+            <mesh
+               geometry={nodes.table_table_mat_0_1.geometry}
+               material={materials.table_mat}
+            />
+         )}
 
-			{isMesh(nodes.table_table_mat_0_2) && (
-				<mesh
-					geometry={nodes.table_table_mat_0_2.geometry}
-					material={materials.computer_mat}
-				>
-					<meshMatcapMaterial map={monitortxt} />
-				</mesh>
-			)}
+         {isMesh(nodes.table_table_mat_0_2) && (
+            <mesh
+               geometry={nodes.table_table_mat_0_2.geometry}
+               material={materials.computer_mat}
+            >
+               <meshMatcapMaterial map={monitortxt} />
+            </mesh>
+         )}
 
-			{isMesh(nodes.table_table_mat_0_3) && (
-				<mesh
-					geometry={nodes.table_table_mat_0_3.geometry}
-					material={materials.server_mat}
-				/>
-			)}
+         {isMesh(nodes.table_table_mat_0_3) && (
+            <mesh
+               geometry={nodes.table_table_mat_0_3.geometry}
+               material={materials.server_mat}
+            />
+         )}
 
-			{isMesh(nodes.table_table_mat_0_4) && (
-				<mesh
-					geometry={nodes.table_table_mat_0_4.geometry}
-					material={materials.vhsPlayer_mat}
-				/>
-			)}
+         {isMesh(nodes.table_table_mat_0_4) && (
+            <mesh
+               geometry={nodes.table_table_mat_0_4.geometry}
+               material={materials.vhsPlayer_mat}
+            />
+         )}
 
-			{isMesh(nodes.table_table_mat_0_5) && (
-				<mesh
-					geometry={nodes.table_table_mat_0_5.geometry}
-					material={materials.stand_mat}
-				/>
-			)}
+         {isMesh(nodes.table_table_mat_0_5) && (
+            <mesh
+               geometry={nodes.table_table_mat_0_5.geometry}
+               material={materials.stand_mat}
+            />
+         )}
 
-			{isMesh(nodes.table_table_mat_0_6) && (
-				<mesh
-					geometry={nodes.table_table_mat_0_6.geometry}
-					material={materials.mat_mat}
-				/>
-			)}
+         {isMesh(nodes.table_table_mat_0_6) && (
+            <mesh
+               geometry={nodes.table_table_mat_0_6.geometry}
+               material={materials.mat_mat}
+            />
+         )}
 
-			{isMesh(nodes.table_table_mat_0_7) && (
-				<mesh
-					geometry={nodes.table_table_mat_0_7.geometry}
-					material={materials.arm_mat}
-				/>
-			)}
+         {isMesh(nodes.table_table_mat_0_7) && (
+            <mesh
+               geometry={nodes.table_table_mat_0_7.geometry}
+               material={materials.arm_mat}
+            />
+         )}
 
-			{isMesh(nodes.table_table_mat_0_8) && (
-				<mesh
-					geometry={nodes.table_table_mat_0_8.geometry}
-					material={materials.tv_mat}
-				>
-					<meshMatcapMaterial map={monitortxt} />
-				</mesh>
-			)}
+         {isMesh(nodes.table_table_mat_0_8) && (
+            <mesh
+               geometry={nodes.table_table_mat_0_8.geometry}
+               material={materials.tv_mat}
+            >
+               <meshMatcapMaterial map={monitortxt} />
+            </mesh>
+         )}
 
-			{isMesh(nodes.table_table_mat_0_9) && (
-				<mesh
-					geometry={nodes.table_table_mat_0_9.geometry}
-					material={materials.cables_mat}
-				/>
-			)}
+         {isMesh(nodes.table_table_mat_0_9) && (
+            <mesh
+               geometry={nodes.table_table_mat_0_9.geometry}
+               material={materials.cables_mat}
+            />
+         )}
 
-			{isMesh(nodes.table_table_mat_0_10) && (
-				<mesh
-					geometry={nodes.table_table_mat_0_10.geometry}
-					material={materials.props_mat}
-				/>
-			)}
+         {isMesh(nodes.table_table_mat_0_10) && (
+            <mesh
+               geometry={nodes.table_table_mat_0_10.geometry}
+               material={materials.props_mat}
+            />
+         )}
 
-			{isMesh(nodes.table_table_mat_0_11) && (
-				<mesh
-					geometry={nodes.table_table_mat_0_11.geometry}
-					material={materials.ground_mat}
-				/>
-			)}
+         {isMesh(nodes.table_table_mat_0_11) && (
+            <mesh
+               geometry={nodes.table_table_mat_0_11.geometry}
+               material={materials.ground_mat}
+            />
+         )}
 
-			{isMesh(nodes.table_table_mat_0_12) && (
-				<mesh
-					geometry={nodes.table_table_mat_0_12.geometry}
-					material={materials.key_mat}
-				/>
-			)}
-		</group>
-	);
+         {isMesh(nodes.table_table_mat_0_12) && (
+            <mesh
+               geometry={nodes.table_table_mat_0_12.geometry}
+               material={materials.key_mat}
+            />
+         )}
+      </group>
+   );
 }
 
 useGLTF.preload('/models/hacker-room.glb');
--- src/components/Target.tsx
+++ src/components/Target.tsx
@@ -5,31 +5,31 @@ import { useRef } from 'react';
 import * as Three from 'three';
 
 function Target(props: { position: Three.Vector3 }) {
-	const targetRef = useRef<Three.Mesh>(null!);
-	const { scene } = useGLTF(
-		'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf'
-	);
+   const targetRef = useRef<Three.Mesh>(null!);
+   const { scene } = useGLTF(
+      'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf'
+   );
 
-	useGSAP(() => {
-		gsap.to(targetRef?.current?.position, {
-			y: targetRef?.current?.position.y + 0.5,
-			duration: 1.5,
-			repeat: -1,
-			yoyo: true,
-		});
-	});
+   useGSAP(() => {
+      gsap.to(targetRef?.current?.position, {
+         y: targetRef?.current?.position.y + 0.5,
+         duration: 1.5,
+         repeat: -1,
+         yoyo: true,
+      });
+   });
 
-	return (
-		// <mesh {...props} ref={(element) => (targetRef as any) = element} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
-		<mesh
-			{...props}
-			ref={targetRef as any}
-			rotation={[0, Math.PI / 5, 0]}
-			scale={1.5}
-		>
-			<primitive object={scene} />
-		</mesh>
-	);
+   return (
+      // <mesh {...props} ref={(element) => (targetRef as any) = element} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
+      <mesh
+         {...props}
+         ref={targetRef}
+         rotation={[0, Math.PI / 5, 0]}
+         scale={1.5}
+      >
+         <primitive object={scene} />
+      </mesh>
+   );
 }
 
 export default Target;
